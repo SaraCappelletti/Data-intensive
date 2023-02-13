14e525ffd5d5fb7b88d0abbc3cb58781409c8f9b
Daniël <d.a.v.ziel@student.tue.nl>
2023-02-13 13:29:18 +0100

    First version query4, needs to be improved

---
+++ query_4.sql
@@ -0,0 +1,19 @@
+SELECT DISTINCT courseoffers.courseofferid
+FROM courseoffers
+INNER JOIN roomallocations
+ON courseoffers.courseofferid = roomallocations.courseofferid
+INNER JOIN rooms
+ON roomallocations.roomid = rooms.roomid
+INNER JOIN courseregistrations
+ON courseoffers.courseofferid = courseregistrations.courseofferid
+
+WHERE courseoffers.year = 2001 --Add %(y)s
+AND courseoffers.quartile = 3 --Add %(q)s
+
+AND (
+	SELECT COUNT(courseregistrations.studentregistrationid) AS cnt
+	FROM courseregistrations
+	WHERE courseoffers.courseofferid = courseregistrations.courseofferid --Add %d	
+) > rooms.roomsize  
+
+ORDER BY courseoffers.courseofferid ASC
