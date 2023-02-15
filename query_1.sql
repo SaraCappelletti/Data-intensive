40390357332eeaef1085f1158bdcb384152633de
Sara Cappelletti <sara.cappelletti4@studio.unibo.it>
2023-02-15 11:52:28 +0100

    Started query 1

---
+++ query_1.sql
@@ -0,0 +1,21 @@
+/*Student administration wants to contact all students that were born in a 
+specified year (parameter %(y)s denotes the year), were registered to a 
+specified degree (parameter %(d)s denotes the degree id), and as part of the 
+course work for that particular degree they attend(ed) any course offer of a 
+specific course (parameter %(c)s denotes the course id). The query should return 
+only the student ids of the students that satisfy this description. The results 
+should be ordered by student id and contain no duplicates. The query will be 
+executed 1000 times with different parameter values. The correct answer should 
+adhere to the following schema: [StudentId int].*/
+
+SELECT s.StudentIs /*miss no duplicates and ordered*/
+FROM Degrees d, 
+JOIN StudentRegistrationsToDegrees srtd 
+ON d.DegreeId = srtd.DegreeID
+JOIN Students s 
+ON s.StudentId = srtd.StudentId 
+JOIN CourseRegistrations cr
+ON cr.StudentRegistrationId = srtd.StudentRegistrationId
+WHERE s.BithyearStudent = ?
+AND d.DegreeId = ?
+AND cr.CourseOfferId = ? /*do I also have to join courses?*/
