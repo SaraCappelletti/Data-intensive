befa13c64982487124c23d31b79d57c8048725f9
Brozn11 <83160056+Brozn11@users.noreply.github.com>
2023-02-15 14:44:14 +0100

    added query 3

---
+++ query_3.sql
@@ -0,0 +1,25 @@
+SELECT 
+  Teachers.BirthyearTeacher, 
+  Teachers.Gender, 
+  Teachers.Salary, 
+  AVG(CourseRegistrations.Grade) AS AverageGrade
+FROM 
+  Teachers
+  JOIN TeacherAssignmentsToCourses 
+    ON Teachers.TeacherId = TeacherAssignmentsToCourses.TeacherId
+  JOIN CourseOffers 
+    ON TeacherAssignmentsToCourses.CourseOfferId = CourseOffers.CourseOfferId
+  JOIN CourseRegistrations 
+    ON CourseOffers.CourseOfferId = CourseRegistrations.CourseOfferId
+WHERE 
+  Teachers.Salary <= 5000
+GROUP BY 
+  CUBE(
+    Teachers.BirthyearTeacher, 
+    Teachers.Gender, 
+    Teachers.Salary
+  )
+ORDER BY 
+  Teachers.BirthyearTeacher, 
+  (CASE WHEN Teachers.Gender='Female' THEN 1 ELSE 2 END), 
+  Teachers.Salary
