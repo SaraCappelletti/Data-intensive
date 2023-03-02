c02e700352b101e40180c401a85e961a62b5ce65
Daniël <d.a.v.ziel@student.tue.nl>
2023-03-02 21:48:09 +0100

    Optimized query 4 to +/- 01:10

--- query_4.sql
+++ query_4.sql
@@ -1,22 +1,21 @@
-SELECT DISTINCT courseoffers.courseofferid, courseoffers.year
+CREATE MATERIALIZED VIEW my_view AS
+SELECT DISTINCT courseoffers.courseofferid, courseoffers.year, courseoffers.quartile
 FROM courseoffers
-
-INNER JOIN roomallocations
-ON courseoffers.courseofferid = roomallocations.courseofferid
-INNER JOIN rooms
-ON roomallocations.roomid = rooms.roomid
-INNER JOIN courseregistrations
-ON courseoffers.courseofferid = courseregistrations.courseofferid
-
+INNER JOIN roomallocations ON courseoffers.courseofferid = roomallocations.courseofferid
+INNER JOIN rooms ON roomallocations.roomid = rooms.roomid
+INNER JOIN courseregistrations ON courseoffers.courseofferid = courseregistrations.courseofferid
 JOIN (
-	SELECT courseofferid, COUNT(studentregistrationid) AS cnt
-	FROM courseregistrations
-	GROUP BY courseofferid
-) cr
-ON courseoffers.courseofferid = cr.courseofferid
-
-WHERE courseoffers.year = %(y)s --Add %(y)s
-AND courseoffers.quartile = %(q)s --Add %(q)s
+    SELECT courseofferid, COUNT(studentregistrationid) AS cnt
+    FROM courseregistrations
+    GROUP BY courseofferid
+) cr ON courseoffers.courseofferid = cr.courseofferid
 AND cr.cnt > rooms.roomsize
+ORDER BY courseoffers.courseofferid ASC; --Takes around a minute and 10 seconds.
 
-ORDER BY courseoffers.courseofferid ASC;
+
+
+SELECT DISTINCT courseofferid, year
+FROM my_view
+WHERE year = 2001
+AND quartile = 3
+ORDER BY courseofferid ASC; --Near Instant execution time
