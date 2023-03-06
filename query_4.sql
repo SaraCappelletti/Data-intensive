83b6fa4eca1a3d6c9d6770cc97ff3d2167258ed1
Daniël <d.a.v.ziel@student.tue.nl>
2023-03-06 19:58:43 +0100

    Improved creation materialized view to +/- 0.6 sec

--- query_4.sql
+++ query_4.sql
@@ -3,20 +3,20 @@ SELECT DISTINCT courseoffers.courseofferid, courseoffers.year, courseoffers.quar
 FROM courseoffers
 INNER JOIN roomallocations ON courseoffers.courseofferid = roomallocations.courseofferid
 INNER JOIN rooms ON roomallocations.roomid = rooms.roomid
-JOIN (
+LEFT JOIN (
     SELECT courseofferid, COUNT(studentregistrationid) AS cnt
     FROM courseregistrations
     GROUP BY courseofferid
 ) cr ON courseoffers.courseofferid = cr.courseofferid
 AND cr.cnt > rooms.roomsize
-ORDER BY courseoffers.courseofferid ASC; --Takes around a minute and 10 seconds.
+ORDER BY courseoffers.courseofferid ASC; --Takes around 15 seconds.
 
 
 SELECT DISTINCT courseofferid, year
 FROM my_view
 WHERE year = %(y)s --Add %(y)s
 AND quartile = %(q)s --Add %(q)s
-ORDER BY courseofferid ASC; -- Takes around 0.05 seconds
+ORDER BY courseofferid ASC; -- Takes around 0.05 seconds.
 
 --Ways to improve performance
 --1. Project only the columns you need
