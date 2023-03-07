d9a574a936868b497707244bf09c199bad5b3717
Daniël <d.a.v.ziel@student.tue.nl>
2023-03-07 21:31:10 +0100

    Fixed query_4

--- build_structures.sql
+++ build_structures.sql
@@ -4,7 +4,7 @@ CREATE INDEX idx_co_courseofferid ON courseoffers (courseofferid);
 CREATE INDEX idx_ra_courseofferid_roomid ON roomallocations (courseofferid, roomid);
 CREATE INDEX idx_cr_courseofferid_studentregistrationid ON courseregistrations (courseofferid, studentregistrationid);
 CREATE INDEX idx_ta_teacherid_courseofferid ON teacherassignmentstocourses (teacherid, courseofferid);
-CREATE INDEX idx_te_teacherid ON teachers (teacherid)
+CREATE INDEX idx_te_teacherid ON teachers (teacherid);
 
 CREATE OR REPLACE VIEW CourseRegistrationWithAvgGrade AS
 SELECT DISTINCT cr.CourseOfferId 
@@ -15,3 +15,19 @@ JOIN (
 	GROUP BY courseofferid
 ) cravg ON cr.CourseOfferId = cravg.CourseOfferID
 JOIN StudentRegistrationsToDegrees srtd ON cr.StudentRegistrationId = srtd.StudentRegistrationId;
+
+CREATE MATERIALIZED VIEW CoursesWithMoreStudentsThanRoom AS
+SELECT courseoffers.courseofferid, courseoffers.year, courseoffers.quartile
+FROM courseoffers
+INNER JOIN (
+	SELECT roomallocations.courseofferid, SUM(roomsize) AS cap
+	FROM rooms, roomallocations
+	WHERE rooms.roomid = roomallocations.roomid
+	GROUP BY roomallocations.courseofferid
+) ro ON ro.courseofferid = courseoffers.courseofferid
+INNER JOIN (
+    SELECT courseofferid, COUNT(studentregistrationid) AS cnt
+    FROM courseregistrations
+    GROUP BY courseofferid
+) cr ON courseoffers.courseofferid = cr.courseofferid
+WHERE cr.cnt > ro.cap;
--- query_4.sql
+++ query_4.sql
@@ -1,26 +1,5 @@
-CREATE MATERIALIZED VIEW my_view AS
-SELECT DISTINCT courseoffers.courseofferid, courseoffers.year, courseoffers.quartile
-FROM courseoffers
-INNER JOIN roomallocations ON courseoffers.courseofferid = roomallocations.courseofferid
-INNER JOIN rooms ON roomallocations.roomid = rooms.roomid
-LEFT JOIN (
-    SELECT courseofferid, COUNT(studentregistrationid) AS cnt
-    FROM courseregistrations
-    GROUP BY courseofferid
-) cr ON courseoffers.courseofferid = cr.courseofferid
-AND cr.cnt > rooms.roomsize
-ORDER BY courseoffers.courseofferid ASC; --Takes around 15 seconds.
-
-
-SELECT DISTINCT courseofferid, year
-FROM my_view
-WHERE year = %(y)s --Add %(y)s
-AND quartile = %(q)s --Add %(q)s
-ORDER BY courseofferid ASC; -- Takes around 0.05 seconds.
-
---Ways to improve performance
---1. Project only the columns you need
---2. Filter early
---3. Make another materialized view
---4. Use indexes
---5. Remove distinct
+SELECT courseofferid
+FROM CoursesWithMoreStudentsThanRoom
+WHERE year = %(y)s 
+AND quartile = %(q)s 
+ORDER BY courseofferid ASC; 
